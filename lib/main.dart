2f93e8d4c64ad03f8fbc1642d460cbb36f312a58
Tejas Ramanujam <101826696+TejasRamanujam@users.noreply.github.com>
2024-02-17 19:18:38 -0600

    Tejas_PrescriptionApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,195 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Health App',
+      home: HealthInfoScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HealthInfoScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HealthInfoScreenState createState() => _HealthInfoScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HealthInfoScreenState extends State<HealthInfoScreen> {
+  int age = 0;
+  int weight = 0;
+  int height = 0;
 
-  void _incrementCounter() {
+  bool isSubmitted = false;
+
+  void updateAge(int newAge) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      age = newAge;
+      if (isSubmitted) {
+        verifyInfo();
+      }
+    });
+  }
+
+  void updateWeight(int newWeight) {
+    setState(() {
+      weight = newWeight;
+      if (isSubmitted) {
+        verifyInfo();
+      }
+    });
+  }
+
+  void updateHeight(int newHeight) {
+    setState(() {
+      height = newHeight;
+      if (isSubmitted) {
+        verifyInfo();
+      }
+    });
+  }
+
+  void verifyInfo() {
+    if (!(weight < 800 || height > 8 || age > 130)) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('You are a medical miracle'),
+            content: Text('The weight must be under 800, the height must be under 8, or the age must be under 130.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void submitInfo() {
+    setState(() {
+      isSubmitted = true;
+      verifyInfo();
+    });
+  }
+
+  void resetSubmit() {
+    setState(() {
+      isSubmitted = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Health App'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text('Please enter your information:', style: TextStyle(fontSize: 18.0)),
+            SizedBox(height: 16.0),
+            HealthInfoForm(updateAge, updateWeight, updateHeight),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: isSubmitted ? resetSubmit : submitInfo,
+              child: Text('Submit'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            SizedBox(height: 32.0),
+            Text('List of prescription medication names:', style: TextStyle(fontSize: 18.0)),
+            SizedBox(height: 16.0),
+            MedicationList(age: age, weight: weight, height: height, isSubmitted: isSubmitted),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HealthInfoForm extends StatelessWidget {
+  final Function(int) onAgeChanged;
+  final Function(int) onWeightChanged;
+  final Function(int) onHeightChanged;
+
+  HealthInfoForm(this.onAgeChanged, this.onWeightChanged, this.onHeightChanged);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TextFormField(
+          onChanged: (value) => onAgeChanged(int.tryParse(value) ?? 0),
+          decoration: InputDecoration(labelText: 'Age'),
+        ),
+        TextFormField(
+          onChanged: (value) => onWeightChanged(int.tryParse(value) ?? 0),
+          decoration: InputDecoration(labelText: 'Weight (lbs)'),
+        ),
+        TextFormField(
+          onChanged: (value) => onHeightChanged(int.tryParse(value) ?? 0),
+          decoration: InputDecoration(labelText: 'Height (inches)'),
+        ),
+      ],
+    );
+  }
+}
+
+class MedicationList extends StatelessWidget {
+  final List<String> medications = [
+    'Aspirin',
+    'Ibuprofen',
+    'Acetaminophen',
+    'Lisinopril',
+    'Metformin',
+    'Atorvastatin',
+    'Simvastatin',
+    'Levothyroxine',
+    'Amlodipine',
+    'Omeprazole'
+  ];
+
+  final int age;
+  final int weight;
+  final int height;
+  final bool isSubmitted;
+
+  MedicationList({required this.age, required this.weight, required this.height, required this.isSubmitted});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: medications.length,
+      itemBuilder: (context, index) {
+        String medication = medications[index];
+        TextStyle style = TextStyle(color: Colors.black);
+
+        if (isSubmitted && ((weight > 150 && medication == 'Aspirin') ||
+            (height > 40 && medication == 'Levothyroxine') ||
+            (age > 80 && medication == 'Lisinopril'))) {
+          style = TextStyle(color: Colors.red);
+        }
+
+        return ListTile(
+          title: Text(
+            medication,
+            style: style,
+          ),
+        );
+      },
     );
   }
 }
